475d980a80f4978017381ef116252f6bf11ced5a
Joel BANKA <bankajoel@joelbpro.com>
2021-12-21 16:06:59 +0100

    creation du projet

---
+++ EsicSchoolBack/src/main/java/fr/esic/EsicSchoolBackApplication.java
@@ -0,0 +1,13 @@
+package fr.esic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsicSchoolBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsicSchoolBackApplication.class, args);
+	}
+
+}
---
+++ EsicSchoolBack/src/test/java/fr/esic/EsicSchoolBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsicSchoolBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
