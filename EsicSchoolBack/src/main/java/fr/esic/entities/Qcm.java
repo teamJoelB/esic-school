dd0d8f7d03445b30fb50348c8c814a0ce5927502
maxim <maxim@LAPTOP-VBKSSDNJ>
2021-12-23 09:49:14 +0100

    qcm

--- EsicSchoolBack/src/main/java/fr/esic/entities/Qcm.java
+++ EsicSchoolBack/src/main/java/fr/esic/entities/Qcm.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import fr.esic.entities.enums.FormationListe;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
