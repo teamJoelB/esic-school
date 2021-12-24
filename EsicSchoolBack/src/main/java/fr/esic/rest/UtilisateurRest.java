45f5ca6a606e326951c4ac0cfdf3e1b84201f8c8
Joel BANKA <bankajoel@joelbpro.com>
2021-12-24 13:14:32 +0100

    rajout de put user

--- EsicSchoolBack/src/main/java/fr/esic/rest/UtilisateurRest.java
+++ EsicSchoolBack/src/main/java/fr/esic/rest/UtilisateurRest.java
@@ -25,7 +25,6 @@ import fr.esic.repository.UtilisateurRepository;
 import fr.esic.repository.VisiteurRepository;
 import fr.esic.services.MailService;
 
-
 @RestController
 @CrossOrigin("*")
 public class UtilisateurRest {
@@ -34,17 +33,19 @@ public class UtilisateurRest {
 	UtilisateurRepository userRepo;
 	@Autowired
 	VisiteurRepository visitRepo;
-	
+
 	// Vérifier les login et mdp pour se connecter
 	@PostMapping("connexion")
 	public Optional<Utilisateur> connect(@RequestBody Utilisateur p) {
-		
+
 		return userRepo.getByLoginAndPassword(p.getMail(), p.getMdp());
 	}
-	
+
 	@PostMapping("verifmail/{id}")
-	public ResponseEntity<Visiteur> verifUtilisateur(@PathVariable Long id, @RequestBody Visiteur v) throws ResourceNotFoundException {
-		Visiteur vBd = visitRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Visiteur avec ID : " + id + " non trouvé"));
+	public ResponseEntity<Visiteur> verifUtilisateur(@PathVariable Long id, @RequestBody Visiteur v)
+			throws ResourceNotFoundException {
+		Visiteur vBd = visitRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Visiteur avec ID : " + id + " non trouvé"));
 		int tentative = v.getTentative();
 		tentative++;
 		if (vBd.getCode().equals(v.getCode()) && tentative < 4) {
@@ -62,114 +63,115 @@ public class UtilisateurRest {
 			vBd.setTentative(tentative);
 		}
 		final Visiteur vUpdated = visitRepo.save(vBd);
-	    return ResponseEntity.ok(vUpdated);
+		return ResponseEntity.ok(vUpdated);
 	}
-	
+
 	// Fenêtre pour saisir @mail pour obtenir un mdp temporaire
 	@PostMapping("recup_mdp/mail")
 	public void saisirMail(@RequestBody Visiteur v) {
-		
+
 	}
 	/*
-	// Envoi d'un mdp temporaire
-	@PostMapping("recup_mdp")
-	public void recuperationMdp(@RequestBody String mail) {
-		Optional<Utilisateur> user = userRepo.findByMail(mail);
-		int tailleMdp = 8;
-		String mdp = MailService.getRandomStr(tailleMdp);
-		String objet = "Récupération mot de passe - Essic School";
-		String message = "Bonjour, \n\nVous avez effectué une demande de réinitialisation "
-				+ "de mot de passe pour accéder à Esic School. Vous trouverez ci-dessous votre "
-				+ "mot de passe temporaire :\n\t" + mdp + "\nSi vous n'êtes pas l'initiateur "
-				+ "de cette demande, veuillez ignorer ce message.\n"
-				+ "\nCordialement\n\n\t Esic School";
-		u.setMdp(mdp);
-		userRepo.save(u);
-		MailService.sendMail(u.getMail(), objet, message);
-		return u;
-	}*/
-	
+	 * // Envoi d'un mdp temporaire
+	 * 
+	 * @PostMapping("recup_mdp") public void recuperationMdp(@RequestBody String
+	 * mail) { Optional<Utilisateur> user = userRepo.findByMail(mail); int tailleMdp
+	 * = 8; String mdp = MailService.getRandomStr(tailleMdp); String objet =
+	 * "Récupération mot de passe - Essic School"; String message =
+	 * "Bonjour, \n\nVous avez effectué une demande de réinitialisation " +
+	 * "de mot de passe pour accéder à Esic School. Vous trouverez ci-dessous votre "
+	 * + "mot de passe temporaire :\n\t" + mdp +
+	 * "\nSi vous n'êtes pas l'initiateur " +
+	 * "de cette demande, veuillez ignorer ce message.\n" +
+	 * "\nCordialement\n\n\t Esic School"; u.setMdp(mdp); userRepo.save(u);
+	 * MailService.sendMail(u.getMail(), objet, message); return u; }
+	 */
+
 	// Afficher tous les utilisateurs (fonctionnalité Administrateur)
 	@GetMapping("admin/utilisateurs")
-	public Iterable<Utilisateur> getAllUtilisateur(){
+	public Iterable<Utilisateur> getAllUtilisateur() {
 		return userRepo.findAll();
 	}
-	
+
 	// Afficher un utilisateur suivant son id (fonctionnalité Administrateur)
 	@GetMapping("admin/utilisateurs/{id}")
-	public Optional<Utilisateur> getOneUtilisateur (@PathVariable Long id){	
+	public Optional<Utilisateur> getOneUtilisateur(@PathVariable Long id) {
 		return userRepo.findById(id);
 	}
-	
+
 	// Désactiver un utilisateur suivant son id au lieu de la supprimer
 	@PutMapping("utilisateur/{id}")
 	public ResponseEntity<Utilisateur> desactiveUtilisateur(@PathVariable Long id) throws ResourceNotFoundException {
-		Utilisateur u = userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Utilisateur avec ID : " + id + " non trouvé"));
+		Utilisateur u = userRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Utilisateur avec ID : " + id + " non trouvé"));
 		u.setActif(false);
 		u.setAvancementInscrit(-1);
 		final Utilisateur uUpdated = userRepo.save(u);
-	    return ResponseEntity.ok(uUpdated);
+		return ResponseEntity.ok(uUpdated);
 	}
-	
-	//Afficher tous les candidats (visibles par tout le monde sauf les candidats)
+
+	// Afficher tous les candidats (visibles par tout le monde sauf les candidats)
 	@GetMapping("candidats")
-	public Iterable<Utilisateur> getAllCandidat(){
+	public Iterable<Utilisateur> getAllCandidat() {
 		return userRepo.findAllCandidat();
 	}
-	
-	//Afficher tous les respos (fonctionnalité Administrateur)
+
+	// Afficher tous les respos (fonctionnalité Administrateur)
 	@GetMapping("responsables")
-	public Iterable<Utilisateur> getAllResponsable(){
+	public Iterable<Utilisateur> getAllResponsable() {
 		return userRepo.findAllResponsable();
 	}
 
-	//ajout des pdf de l'utilisateur
+	// ajout des pdf de l'utilisateur
 	@PutMapping("inscription/pdf/{id}")
-	public ResponseEntity<Utilisateur> ajoutPdf(@PathVariable Long id, @RequestBody Utilisateur utilisateurDetails) throws ResourceNotFoundException {
+	public ResponseEntity<Utilisateur> ajoutPdf(@PathVariable Long id, @RequestBody Utilisateur utilisateurDetails)
+			throws ResourceNotFoundException {
 		Optional<Utilisateur> b = userRepo.findById(id);
-		if(b.isPresent()) {
-			Utilisateur u = userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Formation avec Qcm : " + id + " non trouvée"));
+		if (b.isPresent()) {
+			Utilisateur u = userRepo.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("Formation avec Qcm : " + id + " non trouvée"));
 			u.setNomPieceid(utilisateurDetails.getNomPieceid());
 			u.setPieceid(utilisateurDetails.getPieceid());
 			u.setNomCv(utilisateurDetails.getNomCv());
-			u.setCv(utilisateurDetails.getCv());	
+			u.setCv(utilisateurDetails.getCv());
 			u.setNomlm(utilisateurDetails.getNomlm());
-			u.setLm(utilisateurDetails.getLm());		
+			u.setLm(utilisateurDetails.getLm());
 			u.setNomddo(utilisateurDetails.getNomddo());
-			u.setDdo(utilisateurDetails.getDdo());		
+			u.setDdo(utilisateurDetails.getDdo());
 			u.setNomrn(utilisateurDetails.getNomrn());
 			u.setRn(utilisateurDetails.getRn());
 			u.setNomhandi(utilisateurDetails.getNomhandi());
 			u.setHandi(utilisateurDetails.getHandi());
-			
+
 			final Utilisateur qUpdated = userRepo.save(u);
-		    return ResponseEntity.ok(qUpdated);
-		}else {
+			return ResponseEntity.ok(qUpdated);
+		} else {
 			System.err.println("QCM non trouvé");
 			return null;
-		}	
-		
-	}	
-	
+		}
+
+	}
+
 	// pour test only
 	@PostMapping("inscription/information")
-	public Utilisateur createUser (@RequestBody Utilisateur u) {
+	public Utilisateur createUser(@RequestBody Utilisateur u) {
 		userRepo.save(u);
 		return u;
 	}
-	
-	
-	//ajout des info de l'utilisateur lors de l'inscription a une formation
+
+	// ajout des info de l'utilisateur lors de l'inscription a une formation
 	@PutMapping("inscription/information/{id}")
-	public ResponseEntity<Utilisateur> modifQcm(@PathVariable Long id, @RequestBody Utilisateur userDetails) throws ResourceNotFoundException {
+	public ResponseEntity<Utilisateur> modifQcm(@PathVariable Long id, @RequestBody Utilisateur userDetails)
+			throws ResourceNotFoundException {
 		Optional<Utilisateur> b = userRepo.findById(id);
-		if(b.isPresent()) {
-			Utilisateur q = userRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Formation avec Qcm : " + id + " non trouvée"));
-			
+		if (b.isPresent()) {
+			Utilisateur q = userRepo.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("Formation avec Qcm : " + id + " non trouvée"));
+
 			q.setCiv(userDetails.getCiv());
 			q.setParcours(userDetails.getParcours());
-			q.setNomUsage(userDetails.getNomUsage());  //////////////////////////////////////////////
-			q.setPrenom(userDetails.getPrenom());   //////////////////////////////////////////////
+			q.setNomUsage(userDetails.getNomUsage()); //////////////////////////////////////////////
+			q.setPrenom(userDetails.getPrenom()); //////////////////////////////////////////////
 			q.setNomNaissance(userDetails.getNomNaissance());
 			q.setDateNaissance(userDetails.getDateNaissance());
 			q.setLieuNaissance(userDetails.getLieuNaissance());
@@ -178,7 +180,7 @@ public class UtilisateurRest {
 			q.setVille(userDetails.getVille());
 			q.setTelFixe(userDetails.getTelFixe());
 			q.setTelPortable(userDetails.getTelPortable());
-			q.setMail(userDetails.getMail());  //////////////////////////////////////////////
+			q.setMail(userDetails.getMail()); //////////////////////////////////////////////
 			q.setNationalité(userDetails.getNationalité());
 			q.setNumSecuSocial(userDetails.getNumSecuSocial());
 			q.setNomRepresentant(userDetails.getNomRepresentant());
@@ -206,12 +208,11 @@ public class UtilisateurRest {
 			q.setDiplome2(userDetails.getDiplome2());
 			q.setDiplome3(userDetails.getDiplome3());
 			q.setDureeExperience(userDetails.getDureeExperience());
-			
-			//langues a mettre
+
+			// langues a mettre
 			q.setNiveauFrancais(userDetails.getNiveauFrancais());
 			q.setNiveauAnglais(userDetails.getNiveauAnglais());
-					
-			
+
 			q.setContactEntreprise(userDetails.getContactEntreprise());
 			q.setNombreContactEntreprise(userDetails.getNombreContactEntreprise());
 			q.setTrouveEntreprise(userDetails.getTrouveEntreprise());
@@ -239,25 +240,18 @@ public class UtilisateurRest {
 			q.setQuestion5(userDetails.getQuestion5());
 			q.setQuestion6(userDetails.getQuestion6());
 			q.setCommentaire(userDetails.getCommentaire());
-					
+
 			final Utilisateur qUpdated = userRepo.save(q);
-		    return ResponseEntity.ok(qUpdated);
-		}else {
+			return ResponseEntity.ok(qUpdated);
+		} else {
 			System.err.println("QCM non trouvé");
 			return null;
-		}}}	
-		
+		}
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
+	@PutMapping("user/{id}")
+	public Utilisateur rajoutIfoUser(@PathVariable Long id, @RequestBody Utilisateur u) {
+		u.setId(id);
+		return userRepo.save(u);
+	}
+}
